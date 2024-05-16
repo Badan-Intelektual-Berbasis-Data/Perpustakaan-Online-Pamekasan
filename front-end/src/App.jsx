b228b6aa1d18cf2809b77b1127c11de7d6f53ac5
defiliafatikasari <defiliafatikasari09@gmail.com>
2024-05-16 10:34:35 +0700

    membuat file utama

---
+++ front-end/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <body>
+      <nav className="bg-gray-800">
+        <h1 className="font-bold, text-white">hello</h1>
+      </nav>
+    </body>
+  );
+}
