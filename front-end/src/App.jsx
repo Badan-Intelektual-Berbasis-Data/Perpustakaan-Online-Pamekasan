24fed147644cfcf9b22621dea990c0833625eea8
mfhaisall <mhfaisal011@gmail.com>
2024-05-23 00:53:21 +0700

    penambahan slide banner

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,12 +3,17 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
 import Kategori from './items/Kategori';
+import Clonebanner from './components/Clonebanner';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 function App() {
   return (
     <div>
       <Navbar />
       <Banner />
+      <Clonebanner />
       <Kategori />
     </div>
   );
--- front-end/src/components/Banner.jsx
+++ front-end/src/components/Banner.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 
 export default function Banner() {
   return (
-    <div className="bg-green-600 text-white py-40">
+    <div className="bg-gray-500 text-white py-40">
       <div className="container mx-auto text-center">
-        <h2 className="text-4xl font-bold mb-2">Selamat Datang di Perpustakaan Kami!</h2>
+        <h2 className="text-4xl font-bold mb-2">Setiap buku yang kita baca adalah anak tangga yang membawa kita lebih tinggi dalam pemahaman dan wawasan hidup</h2>
         <p className="text-lg mb-4">Temukan buku favorit Anda dan banyak lagi.</p>
-        <button className="bg-white text-green-600 px-6 py-2 rounded-full font-semibold hover:bg-gray-200">Jelajahi Sekarang</button>
+        <button className="bg-white text-black px-6 py-2 rounded-full font-semibold hover:bg-gray-200">Jelajahi Sekarang</button>
       </div>
     </div>
   );
