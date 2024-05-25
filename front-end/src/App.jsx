79abcdd1d2f45675b50431d52dedc168be4403e4
defiliafatikasari <defiliafatikasari09@gmail.com>
2024-05-25 15:04:11 +0700

    fix bagian banner

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,17 +3,14 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
 import Kategori from './items/Kategori';
-import Clonebanner from './components/Clonebanner';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Banner />
-      <Clonebanner />
       <Kategori />
     </div>
   );
