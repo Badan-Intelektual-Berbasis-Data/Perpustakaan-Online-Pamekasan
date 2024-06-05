c72a3755f0676717a51a6a9230843ce9fc10b68c
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-06-06 04:04:34 +0700

    Menambahkan requirements.txt

--- backend/server/books/models.py
+++ backend/server/books/models.py
@@ -1,16 +1,12 @@
 from django.db import models
 
-class Author(models.Model):
-    AUTHORITY_TYPE_CHOICES = (
-        ('P', 'P'),
-        ('O', 'O'),
-        ('C', 'C'),
-    )
 
-    author_id = models.IntegerField(primary_key=True, unique=True)
-    author_name = models.CharField(100)
-    author_year = models.CharField(20, null=True, blank=True)
-    authority_type = models.CharField(max_length=1, choices=AUTHORITY_TYPE_CHOICES, null=True, blank=True, default=AUTHORITY_TYPE_CHOICES["p"])
-    auth_list = models.CharField(max_length=20, null=True, blank=True)
-    input_date = models.DateField()
-    last_udpate = models.DateField(null=True, blank=True)
+class Author(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+class Book(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    date_published = models.DateField()
+    date_registered = models.DateTimeField(auto_now_add=True, editable=False)
+
