19dff9bbcc63086d47ed565ba6800dadf1938fc9
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-07-23 08:07:42 +0700

    menormalisasikan data ke dalam bentuk 3NF

--- backend/server/books/models.py
+++ backend/server/books/models.py
@@ -15,11 +15,25 @@ class Categories(models.Model):
         return self.name
 
 
-class Books(models.Model):
+class BookDisplay(models.Model):
     title = models.CharField(max_length=200, unique=True)
     author_id = models.ForeignKey(Authors, on_delete=models.CASCADE)
-    category_id = models.ManyToManyField(Categories)
+    category_id = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    date_published = models.DateField()
+    date_registered = models.DateTimeField(auto_now_add=True, editable=False)
+
+
+
+class Book(models.Model):
+    title_id = models.ForeignKey(BookDisplay, on_delete=models.CASCADE)
     publisher = models.CharField(max_length=255, null=True, blank=True)
+    author_id = models.ForeignKey(Authors, on_delete=models.CASCADE)
+    classification_number = models.IntegerField()
+    languange = models.CharField(50)
+    call_number = models.CharField(30)
+    width = models.IntegerField()
+    height = models.IntegerField()
+    pages = models.IntegerField()
     isbn = models.IntegerField(unique=True, null=True, blank=True)  # 13 Digits
     edition = models.IntegerField(default=1)
     date_published = models.DateField()
@@ -29,5 +43,7 @@ class Books(models.Model):
         return self.title
 
 
-
-
+class bookShelf(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
+    date_registered = models.DateTimeField(auto_now_add=True, editable=False)
--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -79,11 +79,14 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'perpustakaanumum',
+        'USER': 'perpus',
+        'PASSWORD': 'perpus123',
+        'HOST': 'localhost',  # Or the hostname where PostgreSQL is running
+        'PORT': '5432',  # Default PostgreSQL port
+    },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
