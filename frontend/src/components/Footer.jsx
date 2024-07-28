5bfd2d5f0bfe7d7cf2e6fdf4f900bba2f87080f5
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-07-28 20:31:38 +0700

    added footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,28 +1,71 @@
-/* eslint-disable no-unused-vars */
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import React from "react";
+import Container from "./molecules/Container";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faFacebook,
+  faXTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
   return (
-    <div className="bg-gray-400 py-4">
-      <div className="container mx-auto px-4 text-center">
-        <p className="text-sm">Perpustakaan Umum Pamekasan | © Copyright all rights reserved</p>
-        <div className="mt-2 flex justify-center space-x-4">
-          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faFacebook} className="text-white text-2xl" />
-          </a>
-          <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faTwitter} className="text-white text-2xl" />
-          </a>
-          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faInstagram} className="text-white text-2xl" />
-          </a>
-          <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faLinkedin} className="text-white text-2xl" />
-          </a>
+    <Container className="bg-gray-600 flex flex-col gap-y-20 pb-1">
+      <div className="flex w-full justify-between items-center">
+        <div>
+          <div className="flex items-center gap-x-6 shrink-0">
+            <img
+              src="/logo.png"
+              alt="Partner 1"
+              className="w-24 h-24 object-cover rounded-md"
+            />
+            <div className="font-bold text-xl text-white">
+              <h2>Dinas Perpustakaan dan Kearsipan</h2>
+              <h2>Kabupaten Pamekasan</h2>
+            </div>
+          </div>
+
+          {/* contanct */}
+          <div className="text-white text-sm mt-6">
+            <p className="w-[50ch]">
+              Jl. Jokotole No.55, Rw. 05, Barurambat Kota, Kec. Pamekasan,
+              Kabupaten Pamekasan, Jawa Timur 69317
+            </p>
+            {/* <p>Fax. : (0324) 325058</p>
+            <p>Telp. : (0324) 325058</p>
+            <p>WA/Telegram : 0878-5995-5556</p> */}
+          </div>
+          {/* end -contanct */}
+        </div>
+
+        <div className="text-white">
+          <h3 className="font-medium text-lg">Tentang Kami</h3>
+          <p className="w-[50ch] mt-3 text-sm">
+            Kami adalah Dinas Perpustakaan dan Kearsipan Kabupaten Pamekasan
+            yang berkomitmen untuk menyediakan akses informasi dan layanan
+            perpustakaan terbaik bagi masyarakat. Kami berupaya meningkatkan
+            minat baca dan mendukung pelestarian arsip daerah.
+          </p>
         </div>
       </div>
-    </div>
+
+
+      <div className="border-t-[1.5px] border-gray-300 flex justify-between items-center py-8">
+          <p className="text-sm text-white">© 2020 Perpustakaan Umum Pamekasan. All rights reserved.</p>
+          <div className="flex gap-x-6">
+            <FontAwesomeIcon
+              icon={faInstagram}
+              className="text-2xl text-white"
+            />
+            <FontAwesomeIcon
+              icon={faFacebook}
+              className="text-2xl text-white"
+            />
+            <FontAwesomeIcon
+              icon={faXTwitter}
+              className="text-2xl text-white"
+            />
+          </div>
+      </div>
+    </Container>
   );
 }
