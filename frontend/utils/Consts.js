97f3321468b2204c81e08790f169f3835a4eff84
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-08-21 05:26:52 +0700

    ADD: Constans file to make it accessible accross the all components

---
+++ frontend/utils/Consts.js
@@ -0,0 +1,19 @@
+export const backdropVariants = {
+    hidden: {
+      visibility: "hidden",
+      opacity: 0,
+      transition: {
+        duration: 0.3,
+        ease: "easeIn",
+      },
+    },
+
+    visible: {
+      visibility: "visible",
+      opacity: 1,
+      transition: {
+        duration: 0.3,
+        ease: "easeOut",
+      },
+    },
+  };
