e6f7373eb1bffc288299004928df39a12969f5c4
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-08-21 20:03:25 +0700

    UPDATE: Duplicating from original card style
    - Menduplikasi styling dari card yang aslinya

--- frontend/src/components/skeletons/CardSkeleton.jsx
+++ frontend/src/components/skeletons/CardSkeleton.jsx
@@ -1,14 +1,15 @@
+import { motion } from 'framer-motion'
 import React from 'react'
 
 export default function CardSkeleton() {
   return (
-    <div className='flex flex-col justify-between  bg-white w-max rounded-lg shadow-md shrink-0 hover:cursor-pointer h-max'>
+    <motion.div 
+    initial={{x: 80}}
+    className='flex flex-col justify-between  bg-white w-max rounded-lg shadow-md shrink-0 hover:cursor-pointer h-max translate-x-[614px]'>
       <div className='w-[180px] min-h-[250px] max-h-[250px] rounded-sm bg-gray-400 animate-pulse'></div>
-
      
         <div className='min-w-full py-2 bg-gray-400 mt-6 animate-pulse'></div>
         <div className='w-1/2 py-2 bg-gray-400 mt-2 animate-pulse'></div>
-    
-    </div>
+    </motion.div>
   )
 }
