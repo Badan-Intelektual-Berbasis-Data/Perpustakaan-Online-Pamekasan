bec2ec3b19338eabf4955ad0498a6591cc1b8f3d
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-08-22 04:17:51 +0700

    ADD: CardGroupGrid component
    - Menambahlan komponen CardGroupGrid

--- frontend/src/components/molecules/CardGroupGrid.jsx
+++ frontend/src/components/molecules/CardGroupGrid.jsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../atoms/Card";
 
-export default function CardGroupGrid({ title, data=[] }) {
+export default function CardGroupGrid({ title, className="" }) {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const getData = async() => {
+      await fetch(`${import.meta.env.VITE_BASE_API_URL}/books/book/`)
+        .then(res => res.json())
+        .then(data => setData(data))
+    }
+
+    getData()
+  }, [])
+
   return (
-    <div>
+    <div className={className}>
         <h2 className="text-3xl font-semibold mb-8 px-20">{title}</h2>
-        <div className="grid grid-cols-5 px-20 box-border gap-x-12 gap-y-10">
+        <div className="grid grid-cols-6 px-20 box-border gap-x-12 gap-y-10">
           {data &&
             data.map((item, index) => (
               <Card
-                key={index}
-                className="translate-x-[-614px]"
-                
-              />
+              key={index}
+              className="translate-x-[-614px]"
+              title={item.title}
+              author={item.author_name}
+              image={item.book_image_url}
+              desc={item.desc}
+              id={item.id}
+            />
             ))}
         </div>
     </div>
