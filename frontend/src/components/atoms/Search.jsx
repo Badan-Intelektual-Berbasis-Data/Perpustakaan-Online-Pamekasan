316d5180e1933c7410c9b92a9b709e6dbe5232b8
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-08-22 16:43:37 +0700

    UPDATE: added search functionality and fix layout
    - Menambahklan fungsionalitas pencarian buku
    - Memperbaiki layout

--- frontend/src/components/atoms/Search.jsx
+++ frontend/src/components/atoms/Search.jsx
@@ -5,11 +5,12 @@ import React, { useState, useEffect, useMemo } from 'react'
 import { backdropVariants } from '../../../utils/Consts'
 import { Link } from 'react-router-dom'
 
-export default function Search({searchOpen, setSearchOpen}) {
+export default function Search({searchOpen, setSearchOpen, authenticated}) {
 
     const [categories, setCategories] = useState([])
     const [booksData, setBooksData] = useState([])
     const [selectedOption, setSelectedOption] = useState(0);
+    const [searchValue, setSearchValue] = useState("")
 
 
   async function getData(){
@@ -41,7 +42,13 @@ useEffect(() => {
   if (searchOpen){
     getData()
   }
-}, [categories])
+}, [categories, selectedOption])
+
+useEffect(() => {
+  setBooksData(booksData.filter((book) => book.title.includes(searchValue) || book.author_name.includes(searchValue)))
+
+
+}, [searchValue])
   
 
 
@@ -53,7 +60,7 @@ useEffect(() => {
     className="bg-white absolute z-30 top-0 left-0 w-full rounded-md"
   >
     <div
-      className="flex gap-x-20 items-center"
+      className="flex items-center justify-between px-4"
       onBlur={() => setSearchOpen(false)}
     >
       <div className="flex items-center">
@@ -69,25 +76,34 @@ useEffect(() => {
         </div>
       </div>
 
-      <div className="flex-1 flex items-center max-w-[800px] bg-gray-200 rounded-md px-4">
+      <div className="flex-1 flex items-center max-w-[550px] bg-gray-200 rounded-md px-4">
         <FontAwesomeIcon icon={faSearch} className="text-gray-500" />
         <input
           type="text"
           className="border-none outline-none w-full p-2 bg-transparent text-sm"
           placeholder="Cari buku"
+          onChange={(e) => setSearchValue(e.target.value)}
         />
       </div>
 
       
-      <Link to="/profile">
-        <div className="rounded-full">
-          <img
-            className="h-8 w-8 rounded-full"
-            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-            alt=""
-          />
-        </div>
-      </Link>
+      {!authenticated && (
+            <div className="flex gap-x-2">
+              <Link className="text-white bg-black border-white border-2 text-sm px-6 py-2" to="/login">Masuk</Link>
+              <Link className="text-black bg-white border-black border-2 text-sm px-6 py-2" to="/register">Daftar</Link>
+            </div>
+          )}
+          {authenticated && 
+          <Link to="/profile">
+            <div className="rounded-full">
+              <img
+                className="h-8 w-8 rounded-full"
+                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt=""
+              />
+            </div>
+          </Link>
+          }
     </div>
 
     <div className="flex gap-x-20 px-16 py-8 items-start">
@@ -109,6 +125,9 @@ useEffect(() => {
 
       {/* Search Result */}
       <div className="flex-1 flex flex-col gap-y-[20px] text-gray-700 text-base">
+        {!booksData && (
+          <p>Hasil tidak ditemukan</p>
+        )}
         {booksData &&
           booksData.map((book, index) => (
             <button
