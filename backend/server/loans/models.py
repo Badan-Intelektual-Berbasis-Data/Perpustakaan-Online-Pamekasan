23d18fbad9e2de16934c4399fec90d425c3e6110
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-08-24 10:24:15 +0700

    UPDATE: Renamed book_id and user_id ke book and user

--- backend/server/loans/models.py
+++ backend/server/loans/models.py
@@ -4,8 +4,8 @@ from users.models import User
 
 
 class Loan(models.Model):
-    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     fine_ammount = models.BigIntegerField(default=0)
     return_status = models.BooleanField(default=False)
     borrow_date = models.DateTimeField(editable=False, auto_now_add=True)
