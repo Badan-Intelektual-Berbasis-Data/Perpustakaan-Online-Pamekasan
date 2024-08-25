27978239420655510bffb38991eda023fd577249
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-08-25 17:01:00 +0700

    ADD: Lazy load to load a unused components
    - Menambahkan lazy loading ke komponen yang belum digunakan

--- frontend/src/routers/Routers.jsx
+++ frontend/src/routers/Routers.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
-import Home from "../pages/Home";
-import Detail from "../pages/Detail";
-import Profile from "../pages/Profile";
-import Registration from "../pages/auth/Registration";
-import Login from "../pages/auth/Login";
-import Information from "../pages/Information";
-import Search from "../pages/Search";
-import NotFound from "../pages/NotFound";
+
+
+
+const Home = React.lazy(() => import("../pages/Home"))
+const Detail = React.lazy(() => import("../pages/Detail"))
+const Profile = React.lazy(() => import("../pages/Profile"))
+const Registration = React.lazy(() => import("../pages/auth/Registration"))
+const Login = React.lazy(() => import("../pages/auth/Login"))
+const Information = React.lazy(() => import("../pages/Information"))
+const Search = React.lazy(() => import("../pages/Search"))
+const NotFound = React.lazy(() => import("../pages/NotFound"))
 
 export default function Routers() {
   return (
+        <React.Suspense fallback={<div>Loading...</div>}>
     <BrowserRouter>
       <Routes>
+
         <Route path="/" element={<MainLayout />}>
           <Route path="" element={<Home />} />
           <Route path="search/" element={<Search />} />
@@ -28,5 +33,6 @@ export default function Routers() {
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
+      </React.Suspense>
   );
 }
