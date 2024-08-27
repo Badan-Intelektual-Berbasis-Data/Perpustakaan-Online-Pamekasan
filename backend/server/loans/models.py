601e34106e9c2fa29513caf0c52be5e04c2f25f1
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-08-27 07:33:52 +0700

    UPDATE: Changed ForeignKey field into OneToOne field to make sure instance will only have one related model

--- backend/server/loans/models.py
+++ backend/server/loans/models.py
@@ -4,8 +4,8 @@ from users.models import User
 
 
 class Loan(models.Model):
-    book = models.ForeignKey(BookCode, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.OneToOneField(BookCode, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     fine_ammount = models.BigIntegerField(default=0)
     return_status = models.BooleanField(default=False)
     borrow_date = models.DateTimeField(editable=False, auto_now_add=True)
